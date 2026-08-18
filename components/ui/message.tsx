bdf151b37a46f5ec37d8cb1fed8fa6ffa170c512
Nafis Azizi Riza <nafisaziziriza@gmail.com>
2026-08-18 22:22:12 +1000

    feat: migrate message to the ziiz foundations

--- components/ui/message.tsx
+++ components/ui/message.tsx
@@ -22,7 +22,7 @@ function Message({
       data-slot="message"
       data-align={align}
       className={cn(
-        "group/message relative flex w-full min-w-0 gap-2 text-sm data-[align=end]:flex-row-reverse",
+        "group/message relative flex w-full min-w-0 gap-2 text-copy-14 data-[align=end]:flex-row-reverse",
         className
       )}
       {...props}
@@ -35,7 +35,7 @@ function MessageAvatar({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="message-avatar"
       className={cn(
-        "flex w-fit min-w-8 shrink-0 items-center justify-center self-end overflow-hidden rounded-full bg-muted group-has-data-[slot=message-footer]/message:-translate-y-8",
+        "flex w-fit min-w-8 shrink-0 items-center justify-center self-end overflow-hidden rounded-full bg-gray-100 group-has-data-[slot=message-footer]/message:-translate-y-8",
         className
       )}
       {...props}
@@ -61,7 +61,7 @@ function MessageHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="message-header"
       className={cn(
-        "flex max-w-full min-w-0 items-center px-3 text-xs font-medium text-muted-foreground group-has-data-[variant=ghost]/message:px-0",
+        "flex max-w-full min-w-0 items-center px-3 text-label-12 text-gray-900 group-has-data-[variant=ghost]/message:px-0",
         className
       )}
       {...props}
@@ -74,7 +74,7 @@ function MessageFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="message-footer"
       className={cn(
-        "flex max-w-full min-w-0 items-center px-3 text-xs font-medium text-muted-foreground group-has-data-[variant=ghost]/message:px-0 group-data-[align=end]/message:justify-end",
+        "flex max-w-full min-w-0 items-center px-3 text-label-12 text-gray-900 group-has-data-[variant=ghost]/message:px-0 group-data-[align=end]/message:justify-end",
         className
       )}
       {...props}

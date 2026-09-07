e19c34c75bfd31d5b21244e251cd3d04f29b0edf
Nafis Azizi Riza <nafisaziziriza@gmail.com>
2026-09-07 20:52:05 +1000

    remove dependencies

--- apps/www/app/(docs)/dependencies/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsPage, getDocsMetadata } from "@/lib/docs-page"
-
-const slug = ["dependencies"]
-
-export const metadata = getDocsMetadata(slug)
-
-export default function Page() {
-  return <DocsPage slug={slug} />
-}
